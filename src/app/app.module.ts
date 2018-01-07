c5b91fa235fb5a9678c915a42d86d40de6427b3e
Phil Custer <custer55@gmail.com>
2018-01-07 00:05:09 -0500

    Initial setup of home page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeCarouselComponent,
+    HomeContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
