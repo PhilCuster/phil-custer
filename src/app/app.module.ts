ef3bb9e0e534efcc0a2aea27dd4b7757535fcd50
Phil Custer <custer55@gmail.com>
2018-01-07 16:16:39 -0500

    Added routing and carousel to about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-
+// Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
 import { HomeContentComponent } from './home-content/home-content.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
+// Services
+import { PhotosService } from './photos.service';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeContentComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: HomeContentComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeCarouselComponent,
-    HomeContentComponent
+    HomeContentComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ 
+    PhotosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home-carousel/home-carousel.component.ts
+++ src/app/home-carousel/home-carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotosService } from '../photos.service';
 
 @Component({
   selector: 'app-home-carousel',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeCarouselComponent implements OnInit {
 
-  constructor() { }
+  photos: Object[];
+
+  constructor(
+    private photosService: PhotosService
+  ) {
+    photosService.getJSON().subscribe(data => {
+      this.photos = data.user.media.nodes;
+      console.log(this.photos);
+    });
+  }
 
   ngOnInit() {
   }
